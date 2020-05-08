337913c9027cec261b1ca57344a411cbbed4a4fc
Valentina Zapata <valentinazapata@MacBook-Pro-de-Valentina-2.local>
2020-05-08 02:45:40 -0500

    todooo

---
+++ Desktop/REPASO-QUIZ2/Repaso_Quiz2/src/excepciones/BolasException.java
@@ -0,0 +1,10 @@
+package excepciones;
+
+public class BolasException extends Exception {
+
+	public BolasException() {
+		super("Ya no hay elementos");
+
+	}
+
+}
---
+++ Desktop/REPASO-QUIZ2/Repaso_Quiz2/src/excepciones/FinalException.java
@@ -0,0 +1,10 @@
+package excepciones;
+
+public class FinalException extends Exception {
+
+	public FinalException() {
+		super("Se terminó la interacción");
+
+	}
+
+}
